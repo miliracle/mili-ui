1a30f82618f9e66160a5fcb2db7f10234c84c9fc
miliracle <nguyennhathan126@gmail.com>
2023-08-12 16:58:56 +0700

    feat(css): add design tokens

---
+++ src/themes/default.ts
@@ -0,0 +1,16 @@
+export default {
+    colors: {
+        primary: '#121958',
+        secondary: '#B80646',
+        lightGrey: '#EBEDF5',
+        bgColor: '#F8F8FA',
+        white: '#FFFFFF',
+        status: {
+            green: '#25BA18',
+            yellow: '#FFBB0E',
+            red: '#FE2525',
+            expired: 'rgba(0,0,0,0.3)',
+            blue: '#293BDD'
+        }
+    },
+}

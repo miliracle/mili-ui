127a8d89974853ed7bccc22503cdc597905e2c02
miliracle <nguyennhathan126@gmail.com>
2023-09-14 12:58:54 +0700

    feat(icon): add svg icons

--- docs/example-app/src/App.tsx
+++ docs/example-app/src/App.tsx
@@ -4,12 +4,8 @@ import "mili-ui/style.css"
 
 function App() {
   return (
-    <div>
-      <Card img={{
-        src: 'https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
-        alt: 'Blue Orage'
-      }} 
-      title={'Random img'}>Hele</ Card>
+    <div className='mili'>
+      // TODO button
     </div>
   );
 }
